81b710179626e17ee5dd0a45bd37c1a798df1347
Johnny E (Turing) <joao.e@turing.com>
2021-04-04 13:48:23 -0300

    another typo in test.js fixed

--- pages/test.js
+++ pages/test.js
@@ -36,6 +36,7 @@ export default function Test() {
         description="At Turing, we are looking for a Full Stack Developer specializing in React and Node.js who will be responsible for the entire web development process to deliver highly scalable, customer-centric, and performant web applications."
         cover_cta="Apply as React/Node Developer"
         cta_url="page for applying to job"
+        image_cover="../../public/img/Hero image.png"
       />
       <FancyList
         listTitle="Job responsibilities"
