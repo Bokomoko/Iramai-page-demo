16e65431889355b2c8f032506efe1199d236e69c
NamNV <nguyennamcdt1@gmail.com>
2021-04-12 16:51:24 +0700

    drawer

--- component/common/header.js
+++ component/common/header.js
@@ -2,6 +2,7 @@ import styles from './header.module.css';
 import React from "react";
 import className from "classnames";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SideNav from "./sidenav";
 
 function Header() {
     return (
@@ -9,14 +10,18 @@ function Header() {
             <div className={styles.container}>
                 <div className={styles.container_inner}>
                     <div className={styles.left}>
-                        <h1 className={styles.h1}>Turing</h1>
+                        <h1 className={className(styles.h1, styles.menu_label)}>Turing</h1>
+                        <button className={className(styles.menu)}>
+                            <i className="fas fa-bars"/>
+                        </button>
                     </div>
                     <div className={styles.right}>
                         <div className={className(styles.nav_item_container, styles.nav_item, styles.hover_1)}>
                             <span className={className(styles.nav_item_container, styles.nav_item)}>For Companies</span>
                         </div>
                         <div className={className(styles.nav_item_container, styles.nav_item, styles.hover_1)}>
-                            <span className={className(styles.nav_item_container, styles.nav_item)}>For Developers</span>
+                            <span
+                                className={className(styles.nav_item_container, styles.nav_item)}>For Developers</span>
                         </div>
                         <div className={className(styles.nav_item_container, styles.nav_item, styles.hover_1)}>
                             <span className={className(styles.nav_item_container, styles.nav_item)}>Blog</span>
@@ -29,6 +34,7 @@ function Header() {
                     </div>
                 </div>
             </div>
+            <SideNav/>
         </header>
     )
 }
